d8d36c73d47514f40abebbf86758ad05fe251562
jaclynperrone <jaclyn.perrone@gmail.com>
2017-03-31 18:00:47 -0400

    Add middleman.

---
+++ config.rb
@@ -0,0 +1,17 @@
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+set :relative_links, true
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
+set :layout, "layouts/application"
+
+# Per-page layout changes
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+configure :build do
+  activate :relative_assets
+end
