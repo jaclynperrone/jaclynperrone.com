e5f5a93d4b2429654d3ffebb0014cae1b1646450
Jaclyn Perrone <jaclynperrone@Jaclyns-MacBook-Air.local>
2017-10-03 20:59:59 -0400

    Final tweaks to gallery close for medium screens.

--- source/assets/javascripts/gallery-list-item.js
+++ source/assets/javascripts/gallery-list-item.js
@@ -1,11 +1,11 @@
 $(function(){
-  $('.gallery-list__item').click(function(){
+  $('.js-gallery-item').click(function(){
     $('body').addClass('gallery-is-open');
     $(this).addClass('active').siblings().removeClass('active');
   });
 
-  $('.site-header__back').click(function(){
+  $('.js-gallery-close').click(function(){
     $('body').removeClass('gallery-is-open');
-    $('.gallery-list__item').removeClass('active');
+    $('.js-gallery-item').removeClass('active');
   });
 });
