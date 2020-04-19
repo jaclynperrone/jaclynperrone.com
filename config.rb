6349575eab587ff55bd8d543081e629c2bde13f3
Jaclyn Perrone <jaclyn.perrone@gmail.com>
2020-04-19 08:32:52 -0400

    Site overhaul and redesign. Feels sooo good.

--- config.rb
+++ config.rb
@@ -1,32 +1,50 @@
-activate :directory_indexes
-activate :sprockets
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-set :relative_links, true
-set :css_dir, "assets/stylesheets"
-set :js_dir, "assets/javascripts"
-set :images_dir, "assets/images"
-set :layout, "layouts/application"
+# Layouts
+# https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
 configure :development do
-  activate :livereload do |reload|
-    reload.no_swf = true
-  end
+  activate :livereload
 end
 
-configure :build do
-  activate :relative_assets
-end
-
-helpers do
-  def nav_active(path)
-    current_page.path == path ? "active" : ""
-  end
-end
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
--- source/assets/javascripts/application.js
+++
@@ -1,3 +0,0 @@
-//= require jquery
-//= require gallery
-// = require header
